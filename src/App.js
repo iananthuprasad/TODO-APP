24dea3e39fd05ff26ad687171979c6dab4dd3264
iananthuprasad.com <iananthuprasad@gmail.com>
2023-11-05 16:17:08 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import './App.css'
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [added, setAdded] = useState("");
+  const [editindex, setEditindex] = useState(-1);
 
-function App() {
+  const additem = () => {
+    if (added !== "") {
+      setItems([...items, added]);
+      setAdded("");
+    }
+  };
+
+  const itemDelete = (index) => {
+    {
+      /*theezhe ulla indexx  enth veneelum kodukkaaa yaayaaa */
+    }
+
+    const newitem = items.filter((item, i) => i !== index);
+    setItems(newitem);
+    setAdded("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="all">
+        <div className="head">TODO APP</div>
+        <div className="item">
+          <input
+            className="input"
+            type="text"
+            placeholder="enter a new item"
+            value={added}
+            onChange={(e) => setAdded(e.target.value)}
+          ></input>
+          <button className="btn1" onClick={additem}></button>
+
+          <button className="btn2" onClick={() => setItems([])}></button>
+        </div>
+
+        <div>
+          <ul className="ul">
+            {items.map((item, index) => (
+              <li className="list">
+                {editindex === index ? (
+                  <input
+                    className="listinput"
+                    type="text"
+                    value={item}
+                    onChange={(e) => {
+                      {
+                        /* value koduthal input il display */
+                      }
+                      const updatedTaskList = [...items];
+                      updatedTaskList[index] = e.target.value;
+                      setItems(updatedTaskList);
+                    }}
+                  />
+                ) : (
+                  <div className="itemlist">{item}</div>
+                )}
+                {editindex === index ? (
+                  <>
+                    <button onClick={() => setEditindex(-1)}>save</button>
+                  </>
+                ) : (
+                  <>
+                    <button
+                      className="editbtn"
+                      onClick={() => setEditindex(index)}
+                    ></button>
+                 
+                
+                   <button className="editbtn1" onClick={() => itemDelete(index)}></button>
+                   </>
+                )}
+               {" "}
+                {/* argument pass cheyyanel arrow fuction il kodukkanam*/}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default App
